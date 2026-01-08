92d2477a34a423ce90071c55849d97a4965ca670
Aniket Markad <aniket45.markad@gmail.com>
2026-01-09 01:57:19 +0530

    Add files via upload

---
+++ CPP/Assignment15_4.cpp
@@ -0,0 +1,65 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// file name   : Assignment15_4.cpp
+// Description : write a program which Accept number of rows and column from user and display below pattern
+// input.      : iRow = 4, iCol = 4
+// output.     : *  *   *   *
+//               *  @   @   *
+//               *  @   @   *
+//               *  *   *   *
+// Author      : Aniket Devaba Markad                        
+// Date        : 2/1/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+class Pattern
+{
+public:
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : Display
+// Description   : This function is used to display pattern.
+// Author        : Aniket Devaba Markad
+// Date          : 2/1/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if(i == 1 || j == 1 || i == iRow || j == iCol)
+                {
+                    cout << "*\t";
+                }
+                else
+                {
+                    cout << "@\t";
+                }
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    cout << "Enter number of rows\n";
+    cin >> iValue1;
+
+    cout << "Enter number of columns\n";
+    cin >> iValue2;
+
+    Pattern pobj;
+    pobj.Display(iValue1, iValue2);
+
+    return 0;
+}
