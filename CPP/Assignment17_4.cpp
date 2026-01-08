b640db62149ffdefec24c12822540e2be61715ea
Aniket Markad <aniket45.markad@gmail.com>
2026-01-09 01:59:32 +0530

    Add files via upload

---
+++ CPP/Assignment17_4.cpp
@@ -0,0 +1,59 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// file name   : Assignment17_4.cpp
+// Description : write a program which Accept number ofrows and column from user and display below pattern
+// input.      : iRow = 5, iCol = 5
+// output.     : *  *   *   *   *
+//               *  #   #   *   *   
+//               *  #   *   $   *     
+//               *  *   $   $   *           
+//               #  *   *   *   *             
+// Author      : Aniket Devaba Markad                        
+// Date        : 2/1/2026
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+using namespace std;
+
+class Pattern
+{
+public:
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : Display
+// Description   : This function is used to display pattern .
+// Author        : Aniket Devaba Markad 
+// Date          : 2/1/2026 
+// 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    void Display(int iRow,int iCol)
+    {
+        for(int i = iRow; i >= 1; i--)
+        {
+            for(int j = 1; j <= iCol; j++)
+            {
+                if(j == i || i == 1 || j == 1 || j == iCol || i == iRow)
+                    cout << "*\t";
+                else if(i > j)
+                    cout << "#\t";
+                else if(j > i)
+                    cout << "$\t";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    int iValue1, iValue2;
+    cout << "enter the no of rows" << endl;
+    cin >> iValue1;
+    cout << "enter the no of colums" << endl;
+    cin >> iValue2;
+
+    Pattern pobj;
+    pobj.Display(iValue1, iValue2);
+    return 0;
+}
