12c98b0efd84a4b2cb85c9caa18bbccb48838c97
Aniket Markad <aniket45.markad@gmail.com>
2026-01-08 20:42:46 +0530

    Add files via upload

---
+++ CPP/Assignment13_4.cpp
@@ -0,0 +1,58 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// file name   : Assignment13_4.cpp
+// Description : write a program which Accept number ofrows and column from user and display below pattern
+// input.      : iRow = 4, iCol = 5
+// output.     : 4  4   4   4   4
+//               3  3   3   3   3
+//               2  2   2   2   2
+//               1  1   1   1   1
+// Author      : Aniket Devaba Markad
+// Date        : 2/1/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+using namespace std;
+
+class Pattern
+{
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : Display
+// Description   : This function is used to display patttren .
+// Author        : Aniket Devaba Markad
+// Date          : 2/1/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+public:
+    void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = iRow; i >= 1; i--)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                cout << i << "\t";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    cout << "enter the no of rows\n";
+    cin >> iValue1;
+
+    cout << "enter the no of colums\n";
+    cin >> iValue2;
+
+    Pattern pobj;
+    pobj.Display(iValue1, iValue2);
+
+    return 0;
+}
